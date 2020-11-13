8393742c5832310ffc7f818d81eed2533fb4e0c8
InformaticaBiocrucesBizkaia <43874347+InformaticaBiocrucesBizkaia@users.noreply.github.com>
2020-11-13 11:48:09 +0100

    Add files via upload

--- js/mp_functions_folders_1.js
+++ js/mp_functions_folders_1.js
@@ -1,140 +1,168 @@
+	/* Acceso desde el titulo del mapa - El acceso desde el mapa se puede hacer de estas dos maneras diferenciadas */
+	    function func_contenedor_internos_1_folder(){
+	    	$("#bc_mp_procesos").hide();
+	    	$(".contenedor_interno").hide();
+			$("#contenedor_internos_1").show();
+		}
+
+	/* Acceso desde el titulo del mapa - El acceso desde el mapa se puede hacer de estas dos maneras diferenciadas */
+	    function func_contenedor_internos_1_01_folder(){
+	    	$("#bc_mp_procesos").hide();
+	    	$(".contenedor_interno").hide();
+			$("#contenedor_internos_1_01").show();
+		}
+
+		// function func_contenedor_internos_1_02_folder(){
+	 //    	$("#bc_mp_procesos").hide();
+	 //    	$(".contenedor_interno").hide();
+		// 	$("#contenedor_internos_1_02").show();
+	 // 	}
+
+	 	function func_contenedor_internos_1_03_folder(){
+	    	$("#bc_mp_procesos").hide();
+	    	$(".contenedor_interno").hide();
+			$("#contenedor_internos_1_03").show();
+	    }
+
+
 	/* 1_01 - Apartado Procesos estratégicos - Nivel 1 - Plan estrategico*/
-		function func_contenedor_internos_1_01_01(){
+		function func_contenedor_internos_1_01_01_folder(){
 	    	$("#bc_mp_procesos").hide();
 	    	$(".contenedor_interno").hide();
 			$("#contenedor_internos_1_01_01").show();
 		}
-		function func_contenedor_internos_1_01_02(){
+		function func_contenedor_internos_1_01_02_folder(){
 	    	$("#bc_mp_procesos").hide();
 	    	$(".contenedor_interno").hide();
 			$("#contenedor_internos_1_01_02").show();
 		}
-		function func_contenedor_internos_1_01_03(){
+		function func_contenedor_internos_1_01_03_folder(){
 	    	$("#bc_mp_procesos").hide();
 	    	$(".contenedor_interno").hide();
 			$("#contenedor_internos_1_01_03").show();
 		}
-		function func_contenedor_internos_1_01_04(){
+		function func_contenedor_internos_1_01_04_folder(){
 	    	$("#bc_mp_procesos").hide();
 	    	$(".contenedor_interno").hide();
 			$("#contenedor_internos_1_01_04").show();
 		}
-		function func_contenedor_internos_1_01_05(){
+		function func_contenedor_internos_1_01_05_folder(){
 	    	$("#bc_mp_procesos").hide();
 	    	$(".contenedor_interno").hide();
 			$("#contenedor_internos_1_01_05").show();
 		}
-		function func_contenedor_internos_1_01_06(){
+		function func_contenedor_internos_1_01_06_folder(){
 	    	$("#bc_mp_procesos").hide();
 	    	$(".contenedor_interno").hide();
 			$("#contenedor_internos_1_01_06").show();
 		}
-		function func_contenedor_internos_1_01_07(){
+		function func_contenedor_internos_1_01_07_folder(){
 	    	$("#bc_mp_procesos").hide();
 	    	$(".contenedor_interno").hide();
 			$("#contenedor_internos_1_01_07").show();
 		}
-		function func_contenedor_internos_1_01_08(){
+		function func_contenedor_internos_1_01_08_folder(){
 	    	$("#bc_mp_procesos").hide();
 	    	$(".contenedor_interno").hide();
 			$("#contenedor_internos_1_01_08").show();
 		}
-		function func_contenedor_internos_1_01_09(){
+		function func_contenedor_internos_1_01_09_folder(){
 	    	$("#bc_mp_procesos").hide();
 	    	$(".contenedor_interno").hide();
 			$("#contenedor_internos_1_01_09").show();
 		}
 
 	/* 1_02 - Apartado Procesos estratégicos - Nivel 1 - Innovación */
- 		function func_contenedor_internos_1_02_01(){
+ 		function func_contenedor_internos_1_02_01_folder(){
 	    	$("#bc_mp_procesos").hide();
 	    	$(".contenedor_interno").hide();
 			$("#contenedor_internos_1_02_01").show();
 		}
-		function func_contenedor_internos_1_02_02(){
+		function func_contenedor_internos_1_02_02_folder(){
 	    	$("#bc_mp_procesos").hide();
 	    	$(".contenedor_interno").hide();
 			$("#contenedor_internos_1_02_02").show();
 		}
-		function func_contenedor_internos_1_02_03(){
+		function func_contenedor_internos_1_02_03_folder(){
 	    	$("#bc_mp_procesos").hide();
 	    	$(".contenedor_interno").hide();
 			$("#contenedor_internos_1_02_03").show();
 		}
-		function func_contenedor_internos_1_02_04(){
+		function func_contenedor_internos_1_02_04_folder(){
 	    	$("#bc_mp_procesos").hide();
 	    	$(".contenedor_interno").hide();
 			$("#contenedor_internos_1_02_04").show();
 		}
-		function func_contenedor_internos_1_02_05(){
+		function func_contenedor_internos_1_02_05_folder(){
 	    	$("#bc_mp_procesos").hide();
 	    	$(".contenedor_interno").hide();
 			$("#contenedor_internos_1_02_05").show();
 		}
-		function func_contenedor_internos_1_02_06(){
+		function func_contenedor_internos_1_02_06_folder(){
 	    	$("#bc_mp_procesos").hide();
 	    	$(".contenedor_interno").hide();
 			$("#contenedor_internos_1_02_06").show();
 		}
-		function func_contenedor_internos_1_02_07(){
+		function func_contenedor_internos_1_02_07_folder(){
 	    	$("#bc_mp_procesos").hide();
 	    	$(".contenedor_interno").hide();
 			$("#contenedor_internos_1_02_07").show();
 		}
-		function func_contenedor_internos_1_02_08(){
+		function func_contenedor_internos_1_02_08_folder(){
 	    	$("#bc_mp_procesos").hide();
 	    	$(".contenedor_interno").hide();
 			$("#contenedor_internos_1_02_08").show();
 		}
-		function func_contenedor_internos_1_02_09(){
+		function func_contenedor_internos_1_02_09_folder(){
 	    	$("#bc_mp_procesos").hide();
 	    	$(".contenedor_interno").hide();
 			$("#contenedor_internos_1_02_09").show();
 		}
 
 	/* 1_03 - Apartado Procesos estratégicos - Nivel 1 - Orientación y mejora de la calidad */
-    	function func_contenedor_internos_1_03_01(){
+    	function func_contenedor_internos_1_03_01_folder(){
 	    	$("#bc_mp_procesos").hide();
 	    	$(".contenedor_interno").hide();
 			$("#contenedor_internos_1_03_01").show();
 		}
-		function func_contenedor_internos_1_03_02(){
+		function func_contenedor_internos_1_03_02_folder(){
 	    	$("#bc_mp_procesos").hide();
 	    	$(".contenedor_interno").hide();
 			$("#contenedor_internos_1_03_02").show();
 		}
-		function func_contenedor_internos_1_03_03(){
+		function func_contenedor_internos_1_03_03_folder(){
 	    	$("#bc_mp_procesos").hide();
 	    	$(".contenedor_interno").hide();
 			$("#contenedor_internos_1_03_03").show();
 		}
-		function func_contenedor_internos_1_03_04(){
+		function func_contenedor_internos_1_03_04_folder(){
 	    	$("#bc_mp_procesos").hide();
 	    	$(".contenedor_interno").hide();
 			$("#contenedor_internos_1_03_04").show();
 		}
-		function func_contenedor_internos_1_03_05(){
+		function func_contenedor_internos_1_03_05_folder(){
 	    	$("#bc_mp_procesos").hide();
 	    	$(".contenedor_interno").hide();
 			$("#contenedor_internos_1_03_05").show();
 		}
-		function func_contenedor_internos_1_03_06(){
+		function func_contenedor_internos_1_03_06_folder(){
 	    	$("#bc_mp_procesos").hide();
 	    	$(".contenedor_interno").hide();
 			$("#contenedor_internos_1_03_06").show();
 		}
-		function func_contenedor_internos_1_03_07(){
+		function func_contenedor_internos_1_03_07_folder(){
 	    	$("#bc_mp_procesos").hide();
 	    	$(".contenedor_interno").hide();
 			$("#contenedor_internos_1_03_07").show();
 		}
-		function func_contenedor_internos_1_03_08(){
+		function func_contenedor_internos_1_03_08_folder(){
 	    	$("#bc_mp_procesos").hide();
 	    	$(".contenedor_interno").hide();
 			$("#contenedor_internos_1_03_08").show();
 		}
-		function func_contenedor_internos_1_03_09(){
+		function func_contenedor_internos_1_03_09_folder(){
 	    	$("#bc_mp_procesos").hide();
 	    	$(".contenedor_interno").hide();
 			$("#contenedor_internos_1_03_09").show();
 		}
+
